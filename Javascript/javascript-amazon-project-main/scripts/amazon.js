177e1e1b446c4175c0969e4546dc23d53059e536
am-chessman <160284267+am-chessman@users.noreply.github.com>
2024-04-05 14:04:18 +0200

    Created cart.js and made changes:wq

--- Javascript/javascript-amazon-project-main/scripts/amazon.js
+++ Javascript/javascript-amazon-project-main/scripts/amazon.js
@@ -21,7 +21,7 @@ products.forEach((product) => {
             </div>
 
         <div class="product-price">
-            ${product.priceCents / 100}
+           $${product.priceCents / 100}
         </div>
 
         <div class="product-quantity-container">
@@ -46,7 +46,8 @@ products.forEach((product) => {
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id = "${product.id}">
             Add to Cart
         </button>
     </div>`
@@ -55,3 +56,27 @@ console.log(productsHTML);
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId= button.dataset.productId
+   
+        let matchingItem;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item
+            }
+        })
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        }
+        else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            })
+        }
+        console.log(cart)
+    });
+});
