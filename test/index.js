07876a400e0943603cc70337e84b59b78ae002ae
am-chessman <160284267+am-chessman@users.noreply.github.com>
2024-05-11 23:53:48 +0200

    Ended midway, shall continue another day

--- test/index.js
+++ test/index.js
@@ -1,11 +1,10 @@
-import express from "express";
+import express, { request } from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import pg from "pg";
 import bcrypt from "bcrypt";
 
 const app = express();
-const host = "192.168.1.43";
 const port = 3000;
 const __dir = dirname(fileURLToPath(import.meta.url));
 const saltRounds = 10;
@@ -33,20 +32,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log("Attempeting to log in")
-
+    const { email, password } = req.body;
+    
     try {
         const checkCredential = await db.query('SELECT * FROM userinfo WHERE email = $1', [email]);
-        if (checkCredential.rows.length > 0) {
-            const storedEmail = checkCredential.rows[0];
-            const storedPassword = checkCredential.rows[1];
+        const storedUser = checkCredential.rows[0];
 
-            console.log(storedEmail[0])
+        if (checkCredential.rows.length > 0) {
+            const storedEmail = storedUser.email;
+            const storedPassword = storedUser.password;
+
             if (email == storedEmail) {
                 if (password == storedPassword) {
-                    res.redirect('name.ejs')
+                    console.log("Logged In Successfully")
+                    res.render('name.ejs')
                 } else {
                     const wrongPass = "You entered the wrong password";
                     res.send(wrongPass)
@@ -64,58 +63,31 @@ app.get('/signup/email', (req, res) => {
     res.render("email.ejs")
 });
 
-app.post('/signup/age', (req, res) => {
-    res.render('age.ejs')
+app.post('/signup/email', (req, res) => {
+    try {
+        const emailInput = req.body.email;
+        console.log(emailInput)
+    } catch (err) {
+        console.log()
+    }
+    console.log('YEEEEEES')
 })
 
-app.post('/signup/password', async (req, res) => {
-    const password = req.body.password;
-
-    try {
-        if (!password) {
-            return res.render('password.ejs', { error: 'Password is required' });
-        }
-
-        bcrypt.hash(password, saltRounds, async (err, hash) => {
-            if (err) {
-                throw err;
-            } else {
-                console.log("Password hashed successfully: ", hash);
-                await db.query("INSERT INTO userinfo (email, password, dateofbirth, gender) VALUES ($1, $2, $3, $4)", [email, hash, birthday, gender]);
-                console.log("Information inserted successfully");
-                res.send("Information inserted successfully");
-            }
-        });
-    } catch (err) {
-        console.log('An error occurred:', err);
-        res.render('password.ejs', { error: 'An error occurred. Please try again.' });
-    }
+app.post('/signup/age', (req, res) => {
+    res.render('age.ejs');
+    const ageInput = req.body.year +'-'+ req.body.month +'-'+ req.body.day
+    const genderInput = req.body.genderOpt
 });
 
+app.post('/signup/password', (req, res) => {
+    const passwordInput = req.body.newpass;
+    console.log(passwordInput)
+});
 
-app.listen(port, host, () => {
+app.post('/home', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.listen(port, () => {
     console.log("Server running on port:", { port })
 });
-
-
-//try {
-//    if (!password) {
-//        throw new Error('Password is required')
-//    } else {
-
-//        bcrypt.hash(password, saltRounds, async (err, hash) => {
-//            if (err) {
-//                throw err;
-//                console.log("Error hashing password: ", err);
-//            } else {
-//                console.log("Password hashed successfully: ", hash);
-//                await db.query("INSERT INTO userinfo (email, password, dateofbirth, gender) VALUES ($1, $2, $3, $4)", [email, hash, birthday, gender,]);
-//                res.send("Information Inserted successfully");
-//            };
-//        });
-//    }
-
-//} catch (err) {
-//    console.log('An error occurred. Please try again.')
-//    if (err) console.log(err);
-//}
