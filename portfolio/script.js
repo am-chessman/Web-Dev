a67aea269d37505e349fa73abbe0e8f2a868328a
chessman <chessman@world.com>
2024-05-26 17:21:51 +0200

    Updated media queries nav bar

--- portfolio/script.js
+++ portfolio/script.js
@@ -4,9 +4,12 @@ const navBar = document.querySelector('.nav-bar');
 menuIcon.addEventListener('click', () => {
     if (!navBar.classList.contains('js-nav-bar')) {
         navBar.classList.add('js-nav-bar');
+        menuIcon.innerHTML = '<i class="bi bi-x-circle-fill" style="position: relative; top: 5px; left: 2px;"></i>';
+        navBar.setAttribute('style', 'translate: transformX(100%) !important;');
         console.log("Added")
     } else {
         navBar.classList.remove('js-nav-bar')
+        menuIcon.innerHTML = '&#9776;'
         console.log("Removed")
     }
 });
