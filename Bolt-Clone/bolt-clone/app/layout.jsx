2c82a8fe84142f3ffb82d7819cfab873c18a1753
Mark Wabwire <mark.wabwire@arlandagymnasiet.se>
2024-10-19 08:03:12 +0200

    Made changes

--- Bolt-Clone/bolt-clone/app/layout.jsx
+++ Bolt-Clone/bolt-clone/app/layout.jsx
@@ -1,4 +1,5 @@
 import NavigationBar from "@components/nav";
+import "@styles/globals.css"
 
 export const metadata = {
   title: "Bolt Clone",
@@ -11,7 +12,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
       </head>
-      <body className="">
+      <body className="m-0">
         <main className="app">
           <NavigationBar />        
           {children}
