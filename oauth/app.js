99f1275e287cd8e1025ae8de53679c62c6a4e9fb
am-chessman <am-chessman@github.com>
2024-10-24 08:49:37 +0200

    Completed oauth project and made some changes

--- oauth/app.js
+++ oauth/app.js
@@ -1,18 +1,19 @@
 import express, { urlencoded, } from "express"   
 import helmet from "helmet"
 import "dotenv/config"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
+// import { fileURLToPath } from "url"
+// import { dirname, join } from "path"
 import session from "express-session"
 import { Strategy as GoogleStrategy } from "passport-google-oauth20"
 import { Strategy as LocalStrategy } from "passport-local"
 import passport from "passport"
 import bcrypt from "bcrypt"
-import { Strategy } from "passport-oauth2"
+import flash from "connect-flash"
 
 const app = express()
 const port = process.env.PORT || 3000
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//Unnecessary since inex.html is being rendered by "express.static"
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(helmet())
 app.set("view engine", "ejs")
@@ -25,41 +26,98 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false,
+        maxAge: 24 * 60 * 60 * 1000
     }
 }))
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.errorMessage = req.flash("error")
+    next()
+})
 app.use(passport.initialize())
 app.use(passport.session())
 
 //Validate Login in
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({})
+const hardCodedUser = {
+    email: "chessman@email.com",
+    password: "chessman"
+}
+passport.use("local", new LocalStrategy(
+    {usernameField: "email"},
+    function(email, password, done) {
+        if (email !== hardCodedUser.email) {
+            return done(null, false, {message: "Incorrect Email"})
+        }
+
+        if (password !== hardCodedUser.password) {
+            return done(null, false, {message: "Incorrect password"})
+        }
+
+        return done(null, hardCodedUser)
 }))
 
 passport.serializeUser((user, cb) => {
-    process.nextTick(() => {
-        return cb(null, {
-            id: user.id,
-            username: user.username,
-            picture: user.picture
-        })
-    })
+    cb(null, user)
 })
 
 //Process user data
 passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-        return cb(null, user);
-    });
+    cb(null, user)
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "public/index.html"))
-})
+passport.use("google", new GoogleStrategy ({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log("Google OAuth callback invoked");
+    console.log("Access Token: ", accessToken);
+    console.log("Refresh Token: ", refreshToken)
+    console.log("Profile: ", profile);
+    return cb(null, profile)
+}
+))
+
+// app.get("/", (req, res) => {
+//     res.render("index.ejs")
+// })
 
 // Authenticate user
-app.post("/submit", passport.authenticate("local", {failureRedirect: "/", failureMessage: true}), (req, res) => {
-    res.redirect("/")
+app.post("/submit", passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/failed", 
+    failureMessage: true,
+    failureFlash: true
+}))
+
+app.get(
+    "/auth/google", 
+    passport.authenticate("google", 
+    {
+        scope: ["profile", "email"]
+    }
+))
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", 
+    {
+        successRedirect: "/dashboard",
+        failureRedirect: "/",
+    })
+);  
+
+app.get("/dashboard", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.send("Welcome! You are logged in.");
+    } else {
+        res.redirect("/"); 
+    }
+});
+
+app.get("/failed", (req, res) => {
+    const error = req.flash("error")
+    res.send(`Login failed: ${error.length ? error[0] : "Unknown error"}`)
 })
 
 app.listen(port, () => {
